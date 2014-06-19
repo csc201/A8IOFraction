f141e33164f9d61dc1eb7f5279a5b56d41e862fd
tkanchanawanchai6403 <tkanchanawanchai6403@AN093564-CT234.nvstu.edu>
2014-06-19 09:14:42 -0400

    initial

---
+++ A8IOFraction/src/FractionDemo.java
@@ -0,0 +1,52 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+public class FractionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*		
+		Fraction frac1 = new Fraction(1,2);
+		//int x=5;
+		
+		try {
+			FileOutputStream outFile = new FileOutputStream("SerialF.dat");
+			ObjectOutputStream objOutput = new ObjectOutputStream(outFile);
+			objOutput.writeObject(frac1);
+			//objOutput.writeInt(x);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+
+		FileInputStream inFile;
+		try {
+			inFile = new FileInputStream("SerialF.dat");
+			ObjectInputStream objInput = new ObjectInputStream(inFile);
+			Object obj = objInput.readObject();
+			System.out.println((Fraction)obj);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("No Fraction class found!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		
+	}
+
+}
